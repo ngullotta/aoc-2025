9bfeced3ec8cc340fefcb766c09b5ed22a8ab43f
ngullotta <nikolaus.gullotta@protonmail.com>
2025-12-04 19:52:11 -0600

    Day 4

---
+++ 4.cpp
@@ -0,0 +1,54 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+static int64_t mark(std::vector<std::string> &map) {
+  int64_t count{};
+  for (size_t i = 1; i < map.size() - 1; i++)
+    for (size_t j = 1; j < map.front().size() - 1; j++)
+      if (map[i][j] != '.') {
+        int neighbors = 0;
+        for (size_t k = i - 1; k <= i + 1; k++)
+          for (size_t l = j - 1; l <= j + 1; l++)
+            neighbors += map[k][l] != '.';
+        if (neighbors < 5) {
+          ++count;
+          map[i][j] = 'x';
+        }
+      }
+  return count;
+}
+
+static void sweep(std::vector<std::string> &map) {
+  for (size_t i = 1; i < map.size() - 1; i++)
+    for (size_t j = 1; j < map.front().size() - 1; j++)
+      if (map[i][j] == 'x')
+        map[i][j] = '.';
+}
+
+int main() {
+  std::ifstream input{"4.txt"};
+  int64_t part1{}, part2{};
+
+  std::vector<std::string> map;
+  for (std::string line; std::getline(input, line);) {
+    if (map.empty())
+      map.emplace_back(line.size() + 2, '.');
+
+    map.emplace_back(".");
+    map.back() += line;
+    map.back() += '.';
+  }
+  map.emplace_back(map.front());
+
+  part1 = part2 = mark(map);
+
+  int64_t removals;
+  do {
+    sweep(map);
+    part2 += removals = mark(map);
+  } while (removals);
+
+  std::cout << "part1: " << part1 << "\npart2: " << part2 << '\n';
+  return 0;
+}
